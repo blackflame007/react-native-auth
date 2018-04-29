2d9370a44b219ec5c42f3ed4e8e6577dce0d5a65
Nick L <lesseosnick@gmail.com>
2018-04-29 14:02:15 -0700

    added password and email input components

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,18 +2,28 @@ import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-state = { text: '' };
+    state = { email: '', password: '' };
 
 render() {
   return (
     <Card>
         <CardSection>
-            <Input 
-                value={this.state.text}
-                onChangeText={text => this.setState({ text })}
+            <Input
+                placeholder="User@gmail.com"
+                label="Email"
+                value={this.state.email}
+                onChangeText={email => this.setState({ email })}
             />
         </CardSection>
-        <CardSection />
+        <CardSection >
+            <Input
+                secureTextEntry
+                placeholder="password"
+                label="Password"
+                value={this.state.password}
+                onChangeText={password => this.setState({ password })}
+            />
+        </CardSection >
         <CardSection >
             <Button>
                 Log in
