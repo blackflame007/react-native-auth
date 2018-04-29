b380dfac119ae0b92052e3227b2d16eea74ac3e3
Nick L <lesseosnick@gmail.com>
2018-04-29 15:57:03 -0700

    error message

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-    state = { email: '', password: '' };
+    state = { email: '', password: '', error: '' };
+
+    onButtonPress() {
+        const { email, password } = this.state;
+        
+        this.setState({ error: '' });
+        
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .catch(() => {
+            firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(email, password)
+                .catch(() => {
+                    this.setState({ error: 'Authentication Failed.' });
+                });
+        });
+    }
 
 render() {
   return (
@@ -24,8 +40,11 @@ render() {
                 onChangeText={password => this.setState({ password })}
             />
         </CardSection >
+        <Text style={styles.errorTextStyle}>
+            {this.state.error}
+        </Text>
         <CardSection >
-            <Button>
+            <Button onPress={this.onButtonPress.bind(this)}>
                 Log in
             </Button>
         </CardSection >
@@ -33,5 +52,14 @@ render() {
     );
   }
 }
+
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+};
 
 export default LoginForm;
