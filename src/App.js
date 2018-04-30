e64ad1abef12b0139cb31e6ffa4cba4aa0f25abf
Nick L <lesseosnick@gmail.com>
2018-04-29 20:50:30 -0700

    Log Out Button with spinner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+  
   componentWillMount() {
     firebase.initializeApp({
         apiKey: 'AIzaSyB76P-YK08l8_seL7bWdw5H4JU18hA7kv4',
@@ -14,13 +16,37 @@ class App extends Component {
         storageBucket: 'auth-463fc.appspot.com',
         messagingSenderId: '762726859341'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+  
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button>Log Out</Button>
+          </CardSection>
+        ); 
+
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
   }
   
   render() {
     return (
         <View>
           <Header headerText="Authentication" />
-          <LoginForm />
+          {this.renderContent()}
         </View>
     );
   }
